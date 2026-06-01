27eb53a5ba6b01182e282cb166732d87a30c6518
vkciante-ops <vkciante@gmail.com>
2026-06-01 14:03:19 +0200

    Functions.c

--- Functions.c
+++ Functions.c
@@ -1,208 +1,392 @@
-#define _CRT_SECURE_NO_WARNINGS
-
 #include "header.h"
 
-/* ================= GLOBALE ================= */
-Word* words = NULL;
+/* =========================================================
+   [8] DEFINICIJA GLOBALNIH VARIJABLI
+   ========================================================= */
+Word *words = NULL;
 int wordCount = 0;
 
-/* ================= FILE ================= */
-
+/* =========================================================
+   [1][19][20][22]
+   LOAD WORDS
+   CRUD READ + FILES + ERROR + FSEEK
+   ========================================================= */
 void loadWords() {
-	FILE* f = fopen(WORD_FILE, "r");
-	if (!f) return; /* 14.  Za�tita parametara kod svih funkcija. */
 
-	char buffer[100];
+    FILE *f = fopen(WORD_FILE, "r");
 
-	while (fgets(buffer, sizeof(buffer), f)) {
-		buffer[strcspn(buffer, "\n")] = 0;
+    /* [14][22] zaštita + perror */
+    if (!f) {
+        perror("Greška pri otvaranju datoteke");
+        return;
+    }
 
-		words = realloc(words, (wordCount + 1) * sizeof(Word));
-		words[wordCount].text = malloc(strlen(buffer) + 1);
-		strcpy(words[wordCount].text, buffer);
-		wordCount++;
-	}
+    /* =====================================================
+       [20] fseek ftell rewind
+       ===================================================== */
+    fseek(f, 0, SEEK_END);
 
-	fclose(f);
+    long size = ftell(f);
+
+    rewind(f);
+
+    printf("Veličina datoteke: %ld bajtova\n", size);
+
+    /* =====================================================
+       [15] STATIČKO POLJE
+       ===================================================== */
+    char buffer[100];
+
+    while (fgets(buffer, sizeof(buffer), f)) {
+
+        buffer[strcspn(buffer, "\n")] = 0;
+
+        /* =================================================
+           [16][17] REALLOC
+           ================================================= */
+        Word *temp =
+            realloc(words, (wordCount + 1) * sizeof(Word));
+
+        if (!temp) {
+            perror("Greška realloc");
+            fclose(f);
+            return;
+        }
+
+        words = temp;
+
+        /* =================================================
+           [16][17] MALLOC
+           ================================================= */
+        words[wordCount].text =
+            malloc(strlen(buffer) + 1);
+
+        if (!words[wordCount].text) {
+            perror("Greška malloc");
+            fclose(f);
+            return;
+        }
+
+        strcpy(words[wordCount].text, buffer);
+
+        wordCount++;
+    }
+
+    fclose(f);
 }
 
+/* =========================================================
+   [1][19]
+   SAVE WORDS
+   ========================================================= */
 void saveWords() {
-	FILE* f = fopen(WORD_FILE, "w");
-	if (!f) return;
 
-	for (int i = 0; i < wordCount; i++) {
-		fprintf(f, "%s\n", words[i].text);
-	}
+    FILE *f = fopen(WORD_FILE, "w");
 
-	fclose(f);
+    if (!f) {
+        perror("Greška save");
+        return;
+    }
+
+    for (int i = 0; i < wordCount; i++) {
+        fprintf(f, "%s\n", words[i].text);
+    }
+
+    fclose(f);
 }
 
-/* ================= CRUD ================= */
-
+/* =========================================================
+   [1] CREATE / INSERT
+   ========================================================= */
 void addWord() {
-	char buffer[100];
-	printf("Unesi rijec: ");
-	scanf("%s", buffer);
 
-	words = realloc(words, (wordCount + 1) * sizeof(Word));
-	words[wordCount].text = malloc(strlen(buffer) + 1); /* 17. Koristiti funkcije malloc(), calloc(), realloc(), free() � neku od njih, ako ne i sve.*/
-	strcpy(words[wordCount].text, buffer);
-	wordCount++;
+    char buffer[100];
 
-	saveWords();
+    printf("Unesi riječ: ");
+    scanf("%99s", buffer);
+
+    Word *temp =
+        realloc(words, (wordCount + 1) * sizeof(Word));
+
+    if (!temp) {
+        perror("Greška realloc");
+        return;
+    }
+
+    words = temp;
+
+    words[wordCount].text =
+        malloc(strlen(buffer) + 1);
+
+    if (!words[wordCount].text) {
+        perror("Greška malloc");
+        return;
+    }
+
+    strcpy(words[wordCount].text, buffer);
+
+    wordCount++;
+
+    saveWords();
 }
 
+/* =========================================================
+   [1] UPDATE
+   ========================================================= */
+void updateWord() {
+
+    char oldWord[100];
+    char newWord[100];
+
+    printf("Stara riječ: ");
+    scanf("%99s", oldWord);
+
+    for (int i = 0; i < wordCount; i++) {
+
+        if (strcmp(words[i].text, oldWord) == 0) {
+
+            printf("Nova riječ: ");
+            scanf("%99s", newWord);
+
+            char *temp =
+                realloc(words[i].text,
+                        strlen(newWord) + 1);
+
+            if (!temp) {
+                perror("Greška realloc");
+                return;
+            }
+
+            words[i].text = temp;
+
+            strcpy(words[i].text, newWord);
+
+            saveWords();
+
+            printf("Riječ ažurirana!\n");
+
+            return;
+        }
+    }
+
+    printf("Riječ nije pronađena.\n");
+}
+
+/* =========================================================
+   [1] DELETE
+   ========================================================= */
 void deleteWord() {
-	char buffer[100]; /* 15. Koristiti stati?ki zauzeta polja gdje su potrebna, nikako ne koristiti VLA polja.  */
-	printf("Rijec za brisanje: ");
-	scanf("%s", buffer);
 
-	for (int i = 0; i < wordCount; i++) {
-		if (strcmp(words[i].text, buffer) == 0) {
+    char buffer[100];
 
-			free(words[i].text); /* 18. Sigurno brisanje memorije koja je dinami?ki zauzeta, anuliranje memorijskog prostora,
-							 provjera pokaziva?a kako se ne bi dogodila pogre�ka double free() i anuliranje svih pokaziva?a
-							 koji su bili usmjereni na memorijski prostor koji se dinami?ki zauzeo. */
+    printf("Riječ za brisanje: ");
+    scanf("%99s", buffer);
 
-			for (int j = i; j < wordCount - 1; j++)
-				words[j] = words[j + 1];
+    for (int i = 0; i < wordCount; i++) {
 
-			wordCount--;
-			words = realloc(words, wordCount * sizeof(Word));
+        if (strcmp(words[i].text, buffer) == 0) {
 
-			saveWords();
-			return;
-		}
-	}
+            /* =============================================
+               [18] FREE
+               ============================================= */
+            free(words[i].text);
 
-	printf("Nije pronadeno.\n");
+            for (int j = i; j < wordCount - 1; j++) {
+                words[j] = words[j + 1];
+            }
+
+            wordCount--;
+
+            saveWords();
+
+            printf("Riječ obrisana.\n");
+
+            return;
+        }
+    }
+
+    printf("Riječ nije pronađena.\n");
 }
 
+/* =========================================================
+   [1] READ
+   ========================================================= */
 void listWords() {
-	for (int i = 0; i < wordCount; i++) {
-		printf("%d. %s\n", i + 1, words[i].text);
-	}
+
+    for (int i = 0; i < wordCount; i++) {
+        printf("%d. %s\n", i + 1, words[i].text);
+    }
 }
 
-/* ================= GAME ================= */
+/* =========================================================
+   IGRA
+   ========================================================= */
 
+/* [12] pokazivač */
 char* getRandomWord() {
-	if (wordCount == 0) return NULL;
-	return words[rand() % wordCount].text;
+
+    if (wordCount == 0)
+        return NULL;
+
+    return words[rand() % wordCount].text;
 }
 
+/* =========================================================
+   [6] STATIC
+   ========================================================= */
 void drawHangman(int tries) {
-	static int last = -1; /* 6. Primjena klju?ne rije?i static za globalne i lokalne varijable. */
-	if (last == tries) return;
-	last = tries;
 
-	printf("\nPokusaji: %d\n", tries);
+    static int last = -1;
+
+    if (last == tries)
+        return;
+
+    last = tries;
+
+    printf("Preostalo pokušaja: %d\n", tries);
 }
 
 void playGame() {
-	char* word = getRandomWord();
-	if (!word) return;
 
-	int len = strlen(word);
-	char* guess = calloc(len + 1, 1);
+    char *word = getRandomWord();
 
-	for (int i = 0; i < len; i++) guess[i] = '_';
+    if (!word)
+        return;
 
-	int tries = MAX_TRIES;
-	char c;
+    int len = strlen(word);
 
-	while (tries > 0) {
-		printf("\nRijec: %s\n", guess);
-		drawHangman(tries);
+    /* =====================================================
+       [17] CALLOC
+       ===================================================== */
+    char *guess = calloc(len + 1, 1);
 
-		scanf(" %c", &c);
+    for (int i = 0; i < len; i++)
+        guess[i] = '_';
 
-		int found = 0;
+    int tries = MAX_TRIES;
 
-		for (int i = 0; i < len; i++) {
-			if (word[i] == c) {
-				guess[i] = c;
-				found = 1;
-			}
-		}
+    char c;
 
-		if (!found) tries--;
+    while (tries > 0) {
 
-		if (strcmp(word, guess) == 0) {
-			printf("POBJEDA!\n");
-			break;
-		}
-	}
+        printf("\nRiječ: %s\n", guess);
 
-	free(guess);
+        drawHangman(tries);
+
+        scanf(" %c", &c);
+
+        int found = 0;
+
+        for (int i = 0; i < len; i++) {
+
+            if (word[i] == c) {
+                guess[i] = c;
+                found = 1;
+            }
+        }
+
+        if (!found)
+            tries--;
+
+        if (strcmp(word, guess) == 0) {
+            printf("Pobjeda!\n");
+            break;
+        }
+    }
+
+    free(guess);
 }
-/* ================= SORT ================= */
 
-int compareWords(const void* a, const void* b) {
-	Word* w1 = (Word*)a;
-	Word* w2 = (Word*)b;
-	return strcmp(w1->text, w2->text);
+/* =========================================================
+   [23][26] QSORT + FUNCTION POINTER
+   ========================================================= */
+int compareWords(const void *a, const void *b) {
+
+    Word *w1 = (Word*)a;
+    Word *w2 = (Word*)b;
+
+    return strcmp(w1->text, w2->text);
 }
 
 void sortWords() {
-	qsort(words, wordCount, sizeof(Word), compareWords); /* 23.  Sortiranje � preporuka koristiti ugra?enu qsort() funkciju, ina?e se mo�e koristiti bilo koja funkcija sortiranja.  */
-	saveWords();
+
+    qsort(words,
+          wordCount,
+          sizeof(Word),
+          compareWords);
+
+    saveWords();
+
+    printf("Riječi sortirane.\n");
 }
 
-/* ================= SEARCH ================= */
+/* =========================================================
+   [24][25] PRETRAGA + REKURZIJA
+   ========================================================= */
+int recursiveBinarySearch(char *key,
+                          int left,
+                          int right) {
 
-int recursiveBinarySearch(char* key, int left, int right) {
-	if (left > right) return -1;
+    if (left > right)
+        return -1;
 
-	int mid = (left + right) / 2;
+    int mid = (left + right) / 2;
 
-	int cmp = strcmp(words[mid].text, key);
+    int cmp = strcmp(words[mid].text, key);
 
-	if (cmp == 0) return mid;
-	if (cmp > 0)
-		return recursiveBinarySearch(key, left, mid - 1);
-	else
-		return recursiveBinarySearch(key, mid + 1, right);
+    if (cmp == 0)
+        return mid;
+
+    if (cmp > 0)
+        return recursiveBinarySearch(
+            key,
+            left,
+            mid - 1
+        );
+
+    return recursiveBinarySearch(
+        key,
+        mid + 1,
+        right
+    );
 }
 
 void searchWord() {
-	char buffer[100];
-	printf("Trazi rijec: ");
-	scanf("%s", buffer);
 
-	int res = recursiveBinarySearch(buffer, 0, wordCount - 1);
+    char buffer[100];
 
-	if (res != -1)
-		printf("Nadeno: %s\n", words[res].text);
-	else
-		printf("Nije nadeno.\n");
+    printf("Traži riječ: ");
+
+    scanf("%99s", buffer);
+
+    int result =
+        recursiveBinarySearch(
+            buffer,
+            0,
+            wordCount - 1
+        );
+
+    if (result != -1)
+        printf("Pronađeno: %s\n",
+               words[result].text);
+    else
+        printf("Nije pronađeno.\n");
 }
 
-/* ================= FILE OPS ================= */
-
+/* =========================================================
+   [21] RENAME
+   ========================================================= */
 void backupFile() {
-	FILE* src = fopen(WORD_FILE, "r");
-	FILE* dst = fopen(BACKUP_FILE, "w");
 
-	if (!src || !dst) {
-		perror("Greska"); /* 22. Upravljati s pogre�kama, errno, perror(), strerror(), feof(), ferror() � neku od njih ako ne sve. */
-		return;
-	}
+    rename(WORD_FILE, BACKUP_FILE);
 
-	char c;
-	while ((c = fgetc(src)) != EOF)
-		fputc(c, dst);
-
-	fclose(src); /* 19.  Datoteke, koristiti tekstualnu ili binarnu, provjera pokaziva?a i zatvaranje datoteke. */
-	fclose(dst);
+    printf("Backup napravljen.\n");
 }
 
-void restoreFile() {
-	rename(BACKUP_FILE, WORD_FILE); /* 21. Koristiti funkcije remove(), rename(), po potrebi implementirati funkciju za kopiranje datoteka.*/
-}
-
-/* ================= FUNCTION POINTER ================= */
-
+/* =========================================================
+   [26] POKAZIVAČI NA FUNKCIJE
+   ========================================================= */
 void executeOperation(void (*func)()) {
-	func();
+
+    func();
 }
